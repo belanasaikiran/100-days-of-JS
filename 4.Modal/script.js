54c687587b6628643a98bb7b672ccfb8fb2b4711
Saikiran Belana <saikiransanju22@gmail.com>
2022-11-18 06:14:35 +0530

    Day-4 Modal

---
+++ 4.Modal/script.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal"),
+  modalContent = document.querySelector(".modal-content"),
+  btn = document.querySelector(".btn"),
+  close = document.querySelector(".close");
+
+btn.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modal.addEventListener("click", closeModal);
+
+// open modal:
+function openModal(e) {
+  e.preventDefault();
+  modal.style.display = "block";
+}
+
+function closeModal() {
+    // slide up while closing modal
+    modalContent.classList.add("slide-up")
+    setTimeout(()=> {
+        modal.style.display = "none";
+        modalContent.classList.remove("slide-up")   
+    })
+}
